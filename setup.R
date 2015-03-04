9cec401d693bb93b902ffa851189285fc9c5dc37
Taavi Päll <tapa741@gmail.com>
2015-03-04 16:07:54 +0200

    Migration to Google drive

--- setup.R
+++ setup.R
@@ -2,7 +2,7 @@ library(slidify)
 # setwd("~/Dropbox/Onkobioloogia/")
 # author("p53-ja-apoptoos")
 
-setwd("~/Dropbox/Onkobioloogia/p53-ja-apoptoos")
+setwd("~/Google Drive//Onkobioloogia/p53-ja-apoptoos")
 slidify("index.Rmd")
 browseURL("index.html")
 publish("p53-ja-apoptoos","tpall")
