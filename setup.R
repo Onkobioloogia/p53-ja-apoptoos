a8a172215218fd655e6320633fd0ceed15ebe1b3
Taavi Päll <tapa741@gmail.com>
2015-10-23 18:08:12 +0300

    slight update

--- setup.R
+++ setup.R
@@ -1,8 +1,9 @@
 library(slidify)
+library(slidifyLibraries)
 # setwd("~/Dropbox/Onkobioloogia/")
 # author("p53-ja-apoptoos")
 
-setwd("~/Google Drive//Onkobioloogia/p53-ja-apoptoos")
+setwd("~/Dropbox/Onkobioloogia/p53-ja-apoptoos")
 slidify("index.Rmd")
 browseURL("index.html")
-publish("p53-ja-apoptoos","tpall")
+# publish("p53-ja-apoptoos","tpall")
