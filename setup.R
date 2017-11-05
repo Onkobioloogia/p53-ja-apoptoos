383a3860ae316c43496153fd125924bdbd3b0442
Taavi Päll <tapa741@gmail.com>
2017-11-05 15:19:35 +0200

    cleanup

--- setup.R
+++
@@ -1,9 +0,0 @@
-library(slidify)
-library(slidifyLibraries)
-# setwd("~/Dropbox/Onkobioloogia/")
-# author("p53-ja-apoptoos")
-
-# setwd("~/Dropbox/Onkobioloogia/p53-ja-apoptoos")
-slidify("index.Rmd")
-browseURL("index.html")
-# publish("p53-ja-apoptoos","tpall")
